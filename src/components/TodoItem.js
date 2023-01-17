32c3b54cff50c6898d2bdf7495732e8682c05eda
AmanWorku <amexworku2014@gmail.com>
2023-01-17 11:42:10 +0300

    Create files and setup project

---
+++ src/components/TodoItem.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class TodoItem extends Component {
+	render() {
+		return <li>{this.props.todo.title}</li>;
+	}
+}
---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+
+export default class TodoList extends React.Component {
+	render() {
+		return (
+			<ul>
+				{this.props.todos.map((todo) => (
+					<TodoItem key={todo.id} todo={todo} />
+				))}
+			</ul>
+		);
+	}
+}
