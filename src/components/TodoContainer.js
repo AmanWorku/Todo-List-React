a18191a573847362992c026887e48f808b7810f7
AmanWorku <amexworku2014@gmail.com>
2023-01-17 18:32:05 +0300

    Add todo item file to components

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import TodoList from "./TodoList";
 import Header from "./Header";
+import InputTodo from "./InputTodo";
 class TodoContainer extends React.Component {
 	state = {
 		todos: [
@@ -21,11 +22,54 @@ class TodoContainer extends React.Component {
 			},
 		],
 	};
+
+	handleChange = (id) => {
+		this.setState((prevState) => {
+			return {
+				todos: prevState.todos.map((todo) => {
+					if (todo.id === id) {
+						return {
+							...todo,
+							completed: !todo.completed,
+						};
+					}
+					return todo;
+				}),
+			};
+		});
+	};
+
+	delTodo = (id) => {
+		this.setState({
+			todos: [
+				...this.state.todos.filter((todo) => {
+					return todo.id !== id;
+				}),
+			],
+		});
+	};
+
+	addTodoItem = (title) => {
+		const newTodo = {
+			id: 4,
+			title: title,
+			completed: false,
+		};
+		this.setState({
+			todos: [...this.state.todos, newTodo],
+		});
+	};
+
 	render() {
 		return (
 			<div>
 				<Header />
-				<TodoList todos={this.state.todos} />
+				<InputTodo addTodoProps={this.addTodoItem} />
+				<TodoList
+					todos={this.state.todos}
+					handleChangeProps={this.handleChange}
+					deleteTodoProps={this.delTodo}
+				/>
 			</div>
 		);
 	}
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,14 @@ export default class TodoItem extends Component {
 	render() {
 		return (
 			<li>
-				<input type="checkbox" checked={this.props.todo.completed} />
+				<input
+					type="checkbox"
+					checked={this.props.todo.completed}
+					onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+				/>
+				<button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+					Delete
+				</button>
 				{this.props.todo.title}
 			</li>
 		);
